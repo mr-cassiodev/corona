7ee77b25406fe9246274fe3a80cd92626e9d19c8
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-01 20:55:14 -0300

    components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const HeaderComponent = () => <header>Header</header>;
+import { FiSearch } from 'react-icons/fi';
+
+import { Container, Button, Navigation } from './styles';
+
+const HeaderComponent = () => (
+  <Container>
+    <Button>
+      <Link to="/registrations">
+        <FiSearch size={30} />
+      </Link>
+    </Button>
+    <nav>
+      <ul>
+        <Navigation>
+          <Link to="/">Item</Link>
+        </Navigation>
+        <Navigation>
+          <Link to="/">Item</Link>
+        </Navigation>
+        <Navigation>
+          <Link to="/">Item</Link>
+        </Navigation>
+      </ul>
+    </nav>
+  </Container>
+);
 
 export default HeaderComponent;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const Container = styled.header.attrs({
+  className: 'main-header',
+})`
+  position: absolute;
+  display: flex;
+  z-index: 999;
+  height: 100px;
+  width: 100%;
+  padding: 0 80px;
+
+  justify-content: space-between;
+  align-items: center;
+
+  /* box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.15); */
+  background-color: rgba(0, 0, 0, 0.1);
+
+  a {
+    font-size: 1.8rem;
+    font-weight: 600;
+  }
+
+  nav {
+    &,
+    ul,
+    li,
+    a {
+      display: flex;
+      height: 100%;
+    }
+  }
+`;
+
+export const Button = styled.span`
+  display: flex;
+  width: 60px;
+  height: 60px;
+
+  border-radius: 50%;
+
+  align-items: center;
+  justify-content: center;
+  transition: background-color 300ms ease-in-out;
+
+  :hover {
+    background-color: #071e3d;
+  }
+`;
+
+export const Navigation = styled.li`
+  margin: 0 15px;
+
+  a {
+    position: relative;
+    padding: 0 10px;
+
+    align-items: center;
+    justify-content: center;
+
+    &:before {
+      content: '';
+      position: absolute;
+      width: 0%;
+      height: 3px;
+      bottom: 0;
+      background-color: #071e3d;
+      transition: width 400ms;
+    }
+
+    &:hover:before {
+      width: 100%;
+    }
+  }
+`;
