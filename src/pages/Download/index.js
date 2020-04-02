302672a98ab92dc0ef02ff6f1011c5e4830b81f9
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-04-02 02:36:51 -0300

    update download page
    NOT FINISHED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/pages/Download/index.js
+++ src/pages/Download/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
+import { Container, Content, Section } from './styles';
 
 const DownloadPage = () => (
   <Container>
     <Content>
-      <h1>Versão Móvel !</h1>
-      <p>
-        Baixe agora a versão móvel da nova plataforma do coronavírus e fique por
-        dentro de todas informações na palma da sua mão!
-      </p>
+      <Section>
+        <h1>Download</h1>
+        <p>
+          Baixe agora a versão móvel da nova plataforma do coronavírus e fique
+          por dentro de todas informações na palma da sua mão!
+        </p>
+      </Section>
     </Content>
   </Container>
 );
--- src/pages/Download/styles.js
+++ src/pages/Download/styles.js
@@ -1,23 +1,37 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  height: 100vh;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Content = styled.div`
   display: flex;
-  height: 100%;
   width: 100%;
-  max-width: 1280px;
-  padding: 100px 10px;
-  flex-direction: column;
-  justify-content: center;
+  padding: 96px;
+
+  justify-content: space-between;
   align-items: center;
+`;
+
+export const Section = styled.div`
+  width: 100%;
+  max-width: 380px;
+
   h1 {
+    margin: 50px 0 30px;
     font-size: 7.2rem;
-    text-align: center;
     color: #061a40;
   }
   p {
     font-size: 2.1rem;
-    color: #0353a4;
+    color: #737380;
+    line-height: 30px;
   }
 `;
