a62b2beb4e380fb1cd1aa5792388a90d4f606088
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-02 17:09:48 -0300

    rework

--- src/pages/Download/index.js
+++ src/pages/Download/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-import { Container, Content, Section } from './styles';
+import { Container } from './styles';
 
 const DownloadPage = () => (
   <Container>
-    <Content>
-      <Section>
-        <h1>Download</h1>
-        <p>
-          Baixe agora a versão móvel da nova plataforma do coronavírus e fique
-          por dentro de todas informações na palma da sua mão!
-        </p>
-      </Section>
-    </Content>
+    <h1>Download</h1>
+    <p>
+      Baixe agora a versão móvel da nova plataforma do coronavírus e fique por
+      dentro de todas informações na palma da sua mão!
+    </p>
   </Container>
 );
 
--- src/pages/Download/styles.js
+++ src/pages/Download/styles.js
@@ -1,34 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 1024px;
+  display: flex;
   height: 100vh;
+  width: 100%;
+  max-width: 512px;
   margin: 0 auto;
 
-  display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
-  align-items: center;
-`;
-
-export const Content = styled.div`
-  display: flex;
-  width: 100%;
-  padding: 96px;
-
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const Section = styled.div`
-  width: 100%;
-  max-width: 380px;
 
   h1 {
     margin: 50px 0 30px;
     font-size: 7.2rem;
     color: #061a40;
   }
+
   p {
     font-size: 2.1rem;
     color: #737380;
