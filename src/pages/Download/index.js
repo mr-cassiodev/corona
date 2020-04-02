4e850ff7aeac38d0ca1c948d6c0deeaa02177936
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-04-02 01:45:29 -0300

    .

---
+++ src/pages/Download/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '~/layouts/Default';
+
+import { Container, Content } from './styles';
+
+const DownloadPage = () => (
+  <Layout>
+    <Container>
+      <Content>
+        <h1>CORONAVÍRUS MOBILE</h1>
+      </Content>
+    </Container>
+  </Layout>
+);
